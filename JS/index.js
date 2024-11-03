e156160a1372749ee6461f723fca225c68f592db
Ahmed Refai <momo3956248@gmail.com>
2024-11-03 16:06:17 +0200

    delete search

--- JS/index.js
+++ JS/index.js
@@ -26,29 +26,29 @@ const allData = async (url) => {
   try {
     const response = await fetch(url);
     const data = await response.json();
-
     //! console.log(url === urls.categories);
     if (url === urls.home) {
-      console.log(data.meals);
-      displayhome(data.meals);
+      //! console.log(data.meals);
+      displayhome('home', data.meals);
+      // searchHomeMeal(data.meal);
     } else if (url === urls.categories) {
       //! console.log(data.categories);
-      displayCategories(data.categories);
+      displayhome('categories', data.categories);
     } else if (url === urls.filterCatogry) {
-      console.log(data.meals);
-      displayhome(data.meals);
+      //! console.log(data.meals);
+      displayhome('filterCatogry', data.meals);
     } else if (url === urls.areas) {
       //! console.log(data.meals.length);
-      displayAreas(data.meals);
+      displayhome('areas', data.meals);
     } else if (url === urls.filterAreas) {
-      displayhome(data.meals);
+      displayhome('filterAreas', data.meals);
     } else if (url === urls.Ingredients) {
-      console.log(data.meals[0]);
-      displayIngredients(data.meals);
+      //! console.log(data.meals[0]);
+      displayhome('Ingredients', data.meals);
     } else if (url === urls.filteringredient) {
-      console.log(data.meals);
+      //!console.log(data.meals);
       if (data.meals !== null) {
-        displayhome(data.meals);
+        displayhome('filteringredient', data.meals);
       }
     }
   } catch (error) {
@@ -71,12 +71,14 @@ const details = async (id) => {
   }
   hideLoading();
 };
+
 const searchMeals = async (query) => {
   const searchUrl = `${urls.home}${query}`;
   try {
     showLoading();
     const response = await fetch(searchUrl);
     const data = await response.json();
+    console.log();
     if (data.meals) {
       displayhome(data.meals);
     } else {
@@ -88,11 +90,29 @@ const searchMeals = async (query) => {
     hideLoading();
   }
 };
+
 allData(urls.home);
+
+//*Sidebar-------------------------------*//
+$('.icon-bar i').on('click', function (e) {
+  if ($(this).attr('data-open') === 'true') {
+    console.log('a7a');
+    $('aside').animate({ left: '0px' }, 400);
+    $(this).hide(100, function () {
+      $('.close-menue').css({ display: 'block' });
+    });
+  } else {
+    $('aside').animate({ left: '-312.056px' });
+    $(this).hide(100, function () {
+      $('.open-menue').css({ display: 'block' });
+    });
+  }
+});
 //* Icon close in Details-------------------------- */
 $('.link-website').on('click', function () {
   $('.show-info').hide(100);
   $('.contact').hide();
+  $('.form-search').hide();
   $('.home').show();
   if ($(this).attr('data-target') === 'categories') {
     // hideSection();
@@ -115,81 +135,131 @@ $('.link-website').on('click', function () {
   }
 });
 
-//*Sidebar-------------------------------*//
-$('.icon-bar i').on('click', function (e) {
-  if ($(this).attr('data-open') === 'true') {
-    $('aside').animate({ left: '0px' }, 400);
-    $(this).hide(100, function () {
-      $('.close-menue').css({ display: 'block' });
-    });
-  } else {
-    $('aside').animate({ left: '-312.056px' });
-    $(this).hide(100, function () {
-      $('.open-menue').css({ display: 'block' });
-    });
-  }
-});
+//? let meal;
 
-// *** display Home -------- *//
-
-function displayhome(homeData) {
-  $(row).html('');
-  for (let i = 0; i < homeData.length; i++) {
+function loopdata(data) {
+  for (let i = 0; i < data.length; i++) {
     let meal = `
-      <div class="| col-12 col-md-4 col-lg-3">
-        <div class="inner p-3">
-          <div class="image-foot home-image-meal cursor-p | rounded-4 overflow-hidden position-relative shadow-lg" data-home-id='${homeData[i].idMeal}' onclick='getIdDetails(${homeData[i].idMeal})'>
-            <img src="${homeData[i].strMealThumb}" alt="" class="w-100">
-            <div
-              class="overlay-image position-absolute w-100 h-100 text-center p-2 d-flex justify-content-center align-items-center">
-              <h2 class="fs-bold str-meal">${homeData[i].strMeal}</h2>
-            </div>
+    <div class="| col-12 col-md-4 col-lg-3">
+      <div class="inner p-3">
+        <div class="image-foot home-image-meal cursor-p | rounded-4 overflow-hidden position-relative shadow-lg" data-home-id='${data[i].idMeal}' onclick='getIdDetails(${data[i].idMeal})'>
+          <img src="${data[i].strMealThumb}" alt="" class="w-100">
+          <div
+            class="overlay-image position-absolute w-100 h-100 text-center p-2 d-flex justify-content-center align-items-center">
+            <h2 class="fs-bold str-meal">${data[i].strMeal}</h2>
           </div>
         </div>
       </div>
-      `;
+    </div>
+    `;
     $(row).append(meal);
   }
 }
-// *** display Categotries -------- *//
-function displayCategories(dataCategorie) {
+
+function displayhome(type, data) {
   $(row).html('');
-  for (let i = 0; i < dataCategorie.length; i++) {
-    let categorie = `
-    <div class="| col-12 col-md-4 col-lg-3" onclick='getNameFilter("${dataCategorie[i].strCategory}")'>
-      <div class="inner p-3">
-        <div class="image-foot cursor-p | rounded-5 overflow-hidden position-relative shadow-lg ">
-          <img src="${dataCategorie[i].strCategoryThumb}" alt="" class="w-100">
-          <div class="overlay-image position-absolute w-100 h-100 text-center p-2">
-            <h2 class="fs-bold">${dataCategorie[i].strCategory}</h2>
-            <p>
-              ${dataCategorie[i].strCategoryDescription}</p>
-          </div>
-        </div>
-      </div>
-    </div>`;
-    $(row).append(categorie);
-  }
-}
-function displayAreas(dataArea) {
-  $(row).html('');
-  for (let i = 0; i < dataArea.length; i++) {
-    let areas = `
-    <div class="| col-12 col-md-3 col-lg-2" onclick='getNameArea("${dataArea[i].strArea}")'>
+  if (
+    type === 'home' ||
+    type === 'filterAreas' ||
+    type === 'filteringredient' ||
+    type === 'filterCatogry'
+  ) {
+    loopdata(data);
+  } else if (type === 'categories') {
+    for (let i = 0; i < data.length; i++) {
+      let categorie = `
+          <div class="| col-12 col-md-4 col-lg-3" onclick='getNameFilter("${data[i].strCategory}")'>
             <div class="inner p-3">
               <div class="image-foot cursor-p | rounded-5 overflow-hidden position-relative shadow-lg ">
-                <img
-                  src="imgs/chef cook sticker logo clipart.png"
-                  alt="" class="w-100">
-                <div class="overlay-image bg-white text-black w-100 h-100 text-center p-2">
-                  <h2 class="fs-bold">${dataArea[i].strArea}</h2>
+                <img src="${data[i].strCategoryThumb}" alt="" class="w-100">
+                <div class="overlay-image position-absolute w-100 h-100 text-center p-2">
+                  <h2 class="fs-bold">${data[i].strCategory}</h2>
+                  <p>
+                    ${data[i].strCategoryDescription}</p>
                 </div>
               </div>
             </div>
           </div>`;
-    $(row).append(areas);
+      $(row).append(categorie);
+    }
+  } else if (type === 'areas') {
+    for (let i = 0; i < data.length; i++) {
+      let areas = `
+      <div class="| col-12 col-md-3 col-lg-2" onclick='getNameArea("${data[i].strArea}")'>
+              <div class="inner p-3">
+                <div class="image-foot cursor-p | rounded-5 overflow-hidden position-relative shadow-lg ">
+                  <img
+                    src="imgs/chef cook sticker logo clipart.png"
+                    alt="" class="w-100">
+                  <div class="overlay-image bg-white text-black w-100 h-100 text-center p-2">
+                    <h2 class="fs-bold">${data[i].strArea}</h2>
+                  </div>
+                </div>
+              </div>
+            </div>`;
+      $(row).append(areas);
+    }
+  } else if (type === 'Ingredients') {
+    for (let i = 0; i < 23; i++) {
+      let IngredientCol = `
+        <div class="| col-12 col-md-4 col-lg-3" onclick='getNameFilterIngredients("${
+          data[i].strIngredient
+        }")' >
+              <div class="inner p-3">
+                <div class="image-foot cursor-p | rounded-5 overflow-hidden position-relative shadow-lg ">
+                  <img src="imgs/spaghetti-with-vegetables-cooking-in-a-pan-png.webp" alt="" class="image-Ingredient ">
+                  <div class="text-black bg-white w-100 h-100 text-center p-2">
+                    <h2 class="fs-bold">${data[i].strIngredient}</h2>
+                    <p class='text-muted'>${letterLimit(
+                      data[i].strDescription
+                    )}</p>
+                  </div>
+                </div>
+              </div>
+            </div>`;
+      $(row).append(IngredientCol);
+    }
   }
 }
+// *** display Categotries -------- *//
+// function displayCategories(type, dataCategorie) {
+//   $(row).html('');
+//   for (let i = 0; i < dataCategorie.length; i++) {
+//     let categorie = `
+//     <div class="| col-12 col-md-4 col-lg-3" onclick='getNameFilter("${dataCategorie[i].strCategory}")'>
+//       <div class="inner p-3">
+//         <div class="image-foot cursor-p | rounded-5 overflow-hidden position-relative shadow-lg ">
+//           <img src="${dataCategorie[i].strCategoryThumb}" alt="" class="w-100">
+//           <div class="overlay-image position-absolute w-100 h-100 text-center p-2">
+//             <h2 class="fs-bold">${dataCategorie[i].strCategory}</h2>
+//             <p>
+//               ${dataCategorie[i].strCategoryDescription}</p>
+//           </div>
+//         </div>
+//       </div>
+//     </div>`;
+//     $(row).append(categorie);
+//   }
+// }
+// function displayAreas(type, dataArea) {
+//   $(row).html('');
+//   for (let i = 0; i < dataArea.length; i++) {
+//     let areas = `
+//     <div class="| col-12 col-md-3 col-lg-2" onclick='getNameArea("${dataArea[i].strArea}")'>
+//             <div class="inner p-3">
+//               <div class="image-foot cursor-p | rounded-5 overflow-hidden position-relative shadow-lg ">
+//                 <img
+//                   src="imgs/chef cook sticker logo clipart.png"
+//                   alt="" class="w-100">
+//                 <div class="overlay-image bg-white text-black w-100 h-100 text-center p-2">
+//                   <h2 class="fs-bold">${dataArea[i].strArea}</h2>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>`;
+//     $(row).append(areas);
+//   }
+// }
 function letterLimit(pharagraph) {
   let stringSplit = pharagraph.split(' ');
   let limit = [];
@@ -199,29 +269,30 @@ function letterLimit(pharagraph) {
 
   return limit.join(' ');
 }
-function displayIngredients(ingredients) {
-  $(row).html('');
-  for (let i = 0; i < 23; i++) {
-    let IngredientCol = `
-      <div class="| col-12 col-md-4 col-lg-3" onclick='getNameFilterIngredients("${
-        ingredients[i].strIngredient
-      }")' >
-            <div class="inner p-3">
-              <div class="image-foot cursor-p | rounded-5 overflow-hidden position-relative shadow-lg ">
-                <img src="imgs/spaghetti-with-vegetables-cooking-in-a-pan-png.webp" alt="" class="image-Ingredient ">
-                <div class="text-black bg-white w-100 h-100 text-center p-2">
-                  <h2 class="fs-bold">${ingredients[i].strIngredient}</h2>
-                  <p class='text-muted'>${letterLimit(
-                    ingredients[i].strDescription
-                  )}</p>
-                </div>
-              </div>
-            </div>
-          </div>`;
-    $(row).append(IngredientCol);
-  }
-}
+// function displayIngredients(type, ingredients) {
+//   $(row).html('');
+//   for (let i = 0; i < 23; i++) {
+//     let IngredientCol = `
+//       <div class="| col-12 col-md-4 col-lg-3" onclick='getNameFilterIngredients("${
+//         ingredients[i].strIngredient
+//       }")' >
+//             <div class="inner p-3">
+//               <div class="image-foot cursor-p | rounded-5 overflow-hidden position-relative shadow-lg ">
+//                 <img src="imgs/spaghetti-with-vegetables-cooking-in-a-pan-png.webp" alt="" class="image-Ingredient ">
+//                 <div class="text-black bg-white w-100 h-100 text-center p-2">
+//                   <h2 class="fs-bold">${ingredients[i].strIngredient}</h2>
+//                   <p class='text-muted'>${letterLimit(
+//                     ingredients[i].strDescription
+//                   )}</p>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>`;
+//     $(row).append(IngredientCol);
+//   }
+// }
 //* display Detailes------*//
+
 function displayDetailes(data) {
   $(row).html('');
   let showdetailes = `
@@ -291,7 +362,6 @@ function getNameArea(nameArea) {
   urls.filterAreas = `https://www.themealdb.com/api/json/v1/1/filter.php?a=${nameArea}`;
   allData(urls.filterAreas);
 }
-
 //* function type on click get name gategories------------------*//
 function getNameFilter(nameFilter) {
   console.log(nameFilter);
@@ -305,23 +375,5 @@ function getNameFilterIngredients(nameFilter) {
 }
 
 $('.by-name').on('input', function () {
-  const searchQuery = $(this).val().trim();
-  if (searchQuery) {
-    searchMeals(searchQuery);
-  } else {
-    allData(urls.home);
-  }
-});
-$('.by-first-litter').on('input', function () {
-  let searchQuery = $(this).val().trim();
-  if (searchQuery.length > 1) {
-    searchQuery = searchQuery.substr(0, 1);
-    $(this).val(searchQuery);
-  }
-  if (searchQuery.length === 1) {
-    searchMeals(searchQuery);
-  }
-  if (!searchQuery) {
-    allData(urls.home);
-  }
+  console.log('refai');
 });
